bc4a98c1af6f89c721146174e93f0fb281f0f5a2
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-07-17 19:33:21 +0000

    updated to 2.0.7

--- bin/mvn.bat
+++ bin/mvn.bat
@@ -1,19 +1,21 @@
 @REM ----------------------------------------------------------------------------
-@REM Copyright 2001-2004 The Apache Software Foundation.
-@REM 
-@REM Licensed under the Apache License, Version 2.0 (the "License");
-@REM you may not use this file except in compliance with the License.
-@REM You may obtain a copy of the License at
-@REM 
-@REM      http://www.apache.org/licenses/LICENSE-2.0
-@REM 
-@REM Unless required by applicable law or agreed to in writing, software
-@REM distributed under the License is distributed on an "AS IS" BASIS,
-@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM See the License for the specific language governing permissions and
-@REM limitations under the License.
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+@REM
+@REM    http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied.  See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
 @REM ----------------------------------------------------------------------------
-@REM 
 
 @REM ----------------------------------------------------------------------------
 @REM Maven2 Start Up Batch script
@@ -34,6 +36,9 @@
 @echo off
 @REM enable echoing my setting MAVEN_BATCH_ECHO to 'on'
 @if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
+
+@REM set %HOME% to equivalent of $HOME
+if "%HOME%" == "" (set HOME=%HOMEDRIVE%%HOMEPATH%)
 
 @REM Execute a user defined script before this one
 if exist "%HOME%\mavenrc_pre.bat" call "%HOME%\mavenrc_pre.bat"
@@ -78,7 +83,7 @@ echo.
 goto error
 
 :valMHome
-if exist "%M2_HOME%\bin\m2.bat" goto init
+if exist "%M2_HOME%\bin\mvn.bat" goto init
 
 echo.
 echo ERROR: M2_HOME is set to an invalid directory.
@@ -96,7 +101,7 @@ goto error
 if NOT "%OS%"=="Windows_NT" goto Win9xArg
 
 @REM -- 4NT shell
-if "%eval[2+2]" == "4" goto 4NTArgs
+if "%@eval[2+2]" == "4" goto 4NTArgs
 
 @REM -- Regular WinNT shell
 set MAVEN_CMD_LINE_ARGS=%*
@@ -121,14 +126,25 @@ goto Win9xApp
 :endInit
 SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 
+@REM -- 4NT shell
+if "%@eval[2+2]" == "4" goto 4NTCWJars
+
+@REM -- Regular WinNT shell
+for %%i in ("%M2_HOME%"\boot\classworlds-*) do set CLASSWORLDS_JAR="%%i"
+goto runm2
+
+@REM The 4NT Shell from jp software
+:4NTCWJars
+for %%i in ("%M2_HOME%\boot\classworlds-*") do set CLASSWORLDS_JAR="%%i"
+goto runm2
+
 @REM Start MAVEN2
-for %%i in ("%M2_HOME%"\core\boot\classworlds-*) do set CLASSWORLDS_JAR="%%i"
+:runm2
 %MAVEN_JAVA_EXE% %MAVEN_OPTS% -classpath %CLASSWORLDS_JAR% "-Dclassworlds.conf=%M2_HOME%\bin\m2.conf" "-Dmaven.home=%M2_HOME%" org.codehaus.classworlds.Launcher %MAVEN_CMD_LINE_ARGS%
 if ERRORLEVEL 1 goto error
 goto end
 
 :error
-if "%OS%"=="Windows_NT" @endlocal
 set ERROR_CODE=1
 
 :end
